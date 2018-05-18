e2365d12cba8bc7b6b3eb5001452fa75e4b42c87
Patrick Klostermann <dev@netflair.at>
2018-05-18 19:21:20 +0200

    put movies+= into movi() and calling it in the for-loop

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 //Output DVD's
 var movies = '';
-for (var i = 0; i < dvds.length; i++) {
-	var dvd = dvds[i];
 
-    movies += 
-    "<div class='" + dvd.type + " movies col-lg-3 col-md-4'>" +
+function movi(){
+	 movies += 
+    "<div class='" + dvd.type + " movies col-lg-3 col-md-4 col-sm-6 col-xs-12'>" +
 	    "<div class='media text-center well'>" +
 	    	"<div class='media-heading'>" +
 	    		"<h4>" + dvd.title + "</h4>" +
@@ -12,12 +11,24 @@ for (var i = 0; i < dvds.length; i++) {
 	    	"<div class='media-object'>" +
 	    			"<img class='img-responsive center-block' src='" + dvd.image + "' alt='" + dvd.title + " -dvd Image'>" +
 	    	"</div>" +
-	    	"<div class='media-body'>" +
-	    		"test" +
+	    	"<div class='media-body text-left row'> <hr>" + 		
+	    		"<b>Author: </b>" + dvd.author + " <br> <b>Publisher: </b>" + dvd.publisher +
+	    		"<br><b>Release: </b>" + dvd.release + " <br> <b>Genre: </b>" + dvd.genre +
+		    	"<br><div class='col-lg-6 col-xs-12'>" +
+		    		"<span class='label label-info'>" + dvd.type + "</span>" +
+		    	"</div>" +
+		    	"<div class='col-lg-6 col-xs-12'>" +
+		    		"Rating <br>" +
+		    	"</div>" +
 	    	"</div>" +
 	    "</div>" +
 	"</div>"
     ;
+}
+
+for (var i = 0; i < dvds.length; i++) {
+	var dvd = dvds[i];
+	movi();
 }
 
 $("#movies").html(movies);
