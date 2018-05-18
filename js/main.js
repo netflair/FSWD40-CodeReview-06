6bab5e6c60088aaa5861f1e9529f3004390db222
Patrick Klostermann <dev@netflair.at>
2018-05-18 21:01:26 +0200

    added visual rating system | moved movi() to templates.js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,28 @@
+$(document).ready(function(){
 //Output DVD's
-var movies = '';
-
-function movi(){
-	 movies += 
-    "<div class='" + dvd.type + " movies col-lg-3 col-md-4 col-sm-6 col-xs-12'>" +
-	    "<div class='media text-center well'>" +
-	    	"<div class='media-heading'>" +
-	    		"<h4>" + dvd.title + "</h4>" +
-	    	"</div>" +
-	    	"<div class='media-object'>" +
-	    			"<img class='img-responsive center-block' src='" + dvd.image + "' alt='" + dvd.title + " -dvd Image'>" +
-	    	"</div>" +
-	    	"<div class='media-body text-left row'> <hr>" + 		
-	    		"<b>Author: </b>" + dvd.author + " <br> <b>Publisher: </b>" + dvd.publisher +
-	    		"<br><b>Release: </b>" + dvd.release + " <br> <b>Genre: </b>" + dvd.genre +
-		    	"<br><div class='col-lg-6 col-xs-12'>" +
-		    		"<span class='label label-info'>" + dvd.type + "</span>" +
-		    	"</div>" +
-		    	"<div class='col-lg-6 col-xs-12'>" +
-		    		"Rating <br>" +
-		    	"</div>" +
-	    	"</div>" +
-	    "</div>" +
-	"</div>"
-    ;
-}
-
 for (var i = 0; i < dvds.length; i++) {
-	var dvd = dvds[i];
+	dvd = dvds[i];
 	movi();
 }
 
-$("#movies").html(movies);
 
+$('#movies').html(movies);
+
+
+
+//Rating Stars
+$('.star').hover(function(){
+ $(this).addClass('starred');
+ $(this).prevAll('.star').addClass('starred');
+	}, function(){
+	$(this).removeClass('starred');
+	$(this).prevAll('.star').removeClass('starred');
+});
+
+$('.star').click(function(e) {
+    $(this).addClass('starred-chosen');
+	$(this).prevAll('.star').addClass("starred-chosen");
+});
+
+//---
+ });
