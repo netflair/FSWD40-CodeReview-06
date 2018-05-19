6e49975ed326aaa8627b564d73ebbacda0f3d32f
Patrick Klostermann <dev@netflair.at>
2018-05-19 16:47:34 +0200

    setting variables for input values on click now | added animation to scroll to new item after creation | added functions to toggle show/hide of forms

--- js/main.js
+++ js/main.js
@@ -49,6 +49,149 @@ $(document).ready(function() {
     }
 
 
+
+    //Fire Loop Functions to display items on page load ----------------------
+    dvdLoop();
+    cdLoop();
+    comicLoop();
+
+
+    //PUSHING INTO ARRAY AND OUTPUT -----------------------------------------------------
+    //DVD--------------------------------
+    $('#dvdBtn').on('click', function() {
+        //get values
+        var movTitle = $('#movTitle').val();
+        var movImage = $('#movImage').val();
+        var movAuthor = $('#movAuthor').val();
+        var movGenre = $('#movGenre').val();
+        var movPublisher = $('#movPublisher').val();
+        var movRelease = $('#movRelease').val();
+        var movTrailer = $('#movTrailer').val();
+        var movDesc = $('#movDesc').val();
+        //if Roland Emmerich or roland emmerich show alert
+        if (movAuthor === "Roland Emmerich" || movAuthor === "roland emmerich") {
+            $('#noEmmerich').show();
+            //else proceed
+        } else {
+            $('#noEmmerich').hide();
+            //add object to array
+            dvds.push({
+                "id": "dvd20",
+                "image": "" + movImage + "",
+                "title": "" + movTitle + "",
+                "type": "DVD",
+                "author": "" + movAuthor + "",
+                "genre": "" + movGenre + "",
+                "publisher": "" + movPublisher + "",
+                "release": "" + movRelease + "",
+                "rating": "0",
+                "trailer": "" + movTrailer + "",
+                "desc": "" + movDesc + ""
+            });
+            //output new item
+            movies = '';
+            $('#dvdItems').html(dvdLoop());
+            //scroll to new item
+            $('html, body').animate({
+                scrollTop: $(".dvd20").offset().top - 100
+            }, 1000);
+        }
+    });
+
+    //CD--------------------------------
+    $('#cdBtn').on('click', function() {
+        //get values
+        var cdTitle = $('#cdTitle').val();
+        var cdImage = $('#cdImage').val();
+        var cdAuthor = $('#cdAuthor').val();
+        var cdGenre = $('#cdGenre').val();
+        var cdPublisher = $('#cdPublisher').val();
+        var cdRelease = $('#cdRelease').val();
+        var cdTrailer = $('#cdTrailer').val();
+        var cdDesc = $('#cdDesc').val();
+        //add object to array
+        cds.push({
+            "id": "ost20",
+            "image": "" + cdImage + "",
+            "title": "" + cdTitle + "",
+            "type": "CD",
+            "author": "" + cdAuthor + "",
+            "genre": "" + cdGenre + "",
+            "publisher": "" + cdPublisher + "",
+            "release": "" + cdRelease + "",
+            "rating": "0",
+            "desc": "" + cdDesc + ""
+        });
+        //output new item
+        music = '';
+        $('#cdItems').html(cdLoop());
+        //scroll to new item
+        $('html, body').animate({
+            scrollTop: $(".ost20").offset().top - 100
+        }, 1000);
+    });
+
+    //COMIC-----------------------------
+    $('#comBtn').on('click', function() {
+        //get values
+        var comTitle = $('#comTitle').val();
+        var comImage = $('#comImage').val();
+        var comAuthor = $('#comAuthor').val();
+        var comPages = $('#comPages').val();
+        var comPublisher = $('#comPublisher').val();
+        var comRelease = $('#comRelease').val();
+        var comImgDesc = $('#comImgDesc').val();
+        var comDesc = $('#comDesc').val();
+        //add object to array
+        comics.push({
+            "id": "com20",
+            "image": "" + comImage + "",
+            "title": "" + comTitle + "",
+            "type": "COMIC",
+            "author": "" + comAuthor + "",
+            "pages": "" + comPages + "",
+            "publisher": "" + comPublisher + "",
+            "release": "" + comRelease + "",
+            "rating": "0",
+            "desc": "" + comDesc + "",
+            "descImg": "" + comImgDesc + ""
+        });
+        //output new item
+        draw = '';
+        $('#comicItems').html(comicLoop());
+        //scroll to new item
+        $('html, body').animate({
+            scrollTop: $(".com20").offset().top - 100
+        }, 1000);
+    });
+
+
+    //HIDE SHOW FORMS -------------------------------------------------------
+    $('#dvd-form').hide();
+    $('#cd-form').hide();
+    $('#com-form').hide();
+    //show dvd
+    $('.addDvd').on('click', function() {
+        $('#dvd-form').show();
+        $('#cd-form').hide(400);
+        $('#com-form').hide(400);
+        $('#noEmmerich').hide();
+    });
+    //show cd
+    $('.addCd').on('click', function() {
+        $('#dvd-form').hide(400);
+        $('#cd-form').show();
+        $('#com-form').hide(400);
+    });
+    //show comic
+    $('.addComic').on('click', function() {
+        $('#dvd-form').hide(400);
+        $('#cd-form').hide(400);
+        $('#com-form').show();
+    });
+
+
+
     //Rating Stars --------------------------------------------------------
     $('.star').hover(function() {
         $(this).addClass('starred');
@@ -62,107 +205,6 @@ $(document).ready(function() {
         $(this).addClass('starred-chosen');
         $(this).prevAll('.star').addClass("starred-chosen");
     });
-
-
-    //Fire Loop Functions to display items on page load ----------------------
-    dvdLoop();
-    cdLoop();
-    comicLoop();
-
-
-
-    //Getting Values ----------------------------------------------------------
-    //DVD-------------------------------
-    var movTitle = $('#movTitle').val();
-    var movImage = $('#movImage').val();
-    var movAuthor = $('#movAuthor').val();
-    var movGenre = $('#movGenre').val();
-    var movPublisher = $('#movPublisher').val();
-    var movRelease = $('#movRelease').val();
-    var movTrailer = $('#movTrailer').val();
-    var movDesc = $('#movDesc').val();
-    //CD-------------------------------
-    var cdTitle = $('#cdTitle').val();
-    var cdImage = $('#cdImage').val();
-    var cdAuthor = $('#cdAuthor').val();
-    var cdGenre = $('#cdGenre').val();
-    var cdPublisher = $('#cdPublisher').val();
-    var cdRelease = $('#cdRelease').val();
-    var cdTrailer = $('#cdTrailer').val();
-    var cdDesc = $('#cdDesc').val();
-    //COMICS---------------------------
-    var comTitle = $('#comTitle').val();
-    var comImage = $('#comImage').val();
-    var comAuthor = $('#comAuthor').val();
-    var comPages = $('#comPages').val();
-    var comPublisher = $('#comPublisher').val();
-    var comRelease = $('#comRelease').val();
-    var comImgDesc = $('#comImgDesc').val();
-    var comDesc = $('#comDesc').val();
-
-
-    //PUSHING INTO ARRAY -----------------------------------------------------
-    //DVD--------------------------------
-    $('#dvdBtn').on('click', function() {
-        dvds.push({
-            "id" : "dvd20",
-			"image" : ""+ movImage +"", 
-			"title": ""+ movTitle +"", 
-			"type": "DVD", 
-			"author": ""+ movAuthor +"",
-			"genre": ""+ movGenre +"",
-			"publisher": ""+ movPublisher +"",
-			"release": ""+ movRelease +"",
-			"rating": "0",
-			"trailer": ""+ movTrailer +"",
-			"desc": ""+ movDesc +""
-        });
-        movies = '';
-        $('#dvdItems').html(dvdLoop());
-    });
-
-    //CD--------------------------------
-    $('#cdBtn').on('click', function() {
-        cds.push({
-            "id" : "ost20",
-			"image" : ""+ cdImage +"", 
-			"title": ""+ cdTitle +"", 
-			"type": "CD", 
-			"author": ""+ cdAuthor +"",
-			"genre": ""+ cdGenre +"",
-			"publisher": ""+ cdPublisher +"",
-			"release": ""+ cdRelease +"",
-			"rating": "0",
-			"desc": ""+ cdDesc +""
-        });
-        music = '';
-        $('#cdItems').html(cdLoop());
-    });
-
-    //COMIC-----------------------------
-    $('#comBtn').on('click', function() {
-        comics.push({
-            "id" : "com20",
-			"image" : ""+ comImage +"", 
-			"title": ""+ comTitle +"", 
-			"type": "COMIC", 
-			"author": ""+ comAuthor +"",
-			"pages": ""+ comPages +"",
-			"publisher": ""+ comPublisher +"",
-			"release": ""+ comRelease +"",
-			"rating": "0",
-			"desc": ""+ comDesc +"",
-			"descImg": ""+ comImgDesc +""
-        });
-        draw = '';
-        $('#comicItems').html(comicLoop());
-    });
-
-
-
-
-
-
 
     //--
 });
