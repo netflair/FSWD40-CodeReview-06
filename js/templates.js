8329c59d0f305d28dfb6fcb93617a59c6f04c85d
Patrick Klostermann <dev@netflair.at>
2018-05-19 11:53:07 +0200

    added template for comics

--- js/templates.js
+++ js/templates.js
@@ -134,3 +134,70 @@ function sound(){
 
 }
 
+
+//Comics
+var draw = '';
+var com = '';
+function comic(){
+	 draw += 
+
+	    "<div class='" + com.type + " movies col-lg-3 col-md-4 col-sm-6 col-xs-12'>" +
+	    	//media
+		    "<div class='media text-center well'>" +
+		    	//media-heading
+		    	"<div class='media-heading' data-toggle='modal' data-target='#" + com.id + "'>" +
+		    		"<h4>" + com.title + "</h4><hr>" +
+		    	"</div>" +
+		    	//media-object
+		    	"<div class='media-object' data-toggle='modal' data-target='#" + com.id + "'>" +
+		    			"<img class='img-responsive center-block' src='" + com.image + "' alt='" + com.title + " -dvd Image'>" +
+		    	"</div>" +
+		    	//media-body
+		    	"<div class='media-body text-left row'> <hr>" + 	
+		    		"<b>Writer/Penciller: </b>" + com.author + " <br> <b>Publisher: </b>" + com.publisher +
+		    		"<br><b>Release: </b>" + com.release + " <br> <b>Pages: </b>" + com.pages +
+		    		//label
+			    	"<br><hr><div class='col-xs-3'>" +
+			    		"<span class='label label-success'>" + com.type + "</span>" +
+			    	"</div>" +
+			    	//stars
+			    	"<div class='col-xs-9'>" +
+			    		"<ul class='rating text-right'>" +
+			    			"<li class='star glyphicon glyphicon-star'></li>" +
+			    			"<li class='star glyphicon glyphicon-star'></li>" +
+			    			"<li class='star glyphicon glyphicon-star'></li>" +
+			    			"<li class='star glyphicon glyphicon-star'></li>" +
+			    			"<li class='star glyphicon glyphicon-star'></li>" +
+			    			"<li class='starR'>(" + com.rating + ")</li>" +
+			    		"</ul>" +
+			    	"</div>" +
+
+		    	"</div>" +
+
+		    "</div>" +
+
+		"</div>" +
+	//Modal CD
+	"<div id='" + com.id + "' class='modal fade'>"+
+		"<div class='modal-dialog modal-lg'>" +
+			//modal-content ----------------------------------------
+			"<div class='modal-content'>" +
+				//modal-header
+				"<div class='modal-header'>" +
+					"<button type='button' class='close' data-dismiss='modal'>&times;</button>" +
+          			"<h4 class='modal-title'>" + com.title + "</h4>" +
+          		"</div>" +
+          		//modal-body
+				"<div class='modal-body'>" + com.desc + "<hr>" +
+					"<img class='img-responsive center-block' src='" + com.descImg + "' alt='" + com.title + " - Sample'>" +
+				"</div>" +
+				//modal-footer
+				"<div class='modal-footer'>" +
+					"<button type='button' class='btn btn-default' data-dismiss='modal'>close</button>" +
+          		"</div>" +
+			"</div>" + 
+		"</div>" +
+	"</div>"
+    ;
+
+}
